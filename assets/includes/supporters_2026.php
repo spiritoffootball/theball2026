a595af7f82f0dc66509258c68a46fc098f319110
Christian Wach <needle@haystack.co.uk>
2019-10-19 21:54:49 +0100

    Remove supporters

--- assets/includes/supporters_2026.php
+++ assets/includes/supporters_2026.php
@@ -14,7 +14,7 @@ NOTES
 
 	<div id="sponsor_logos">
 
-		<img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/supporters-2026.png" alt="Our partners" style="padding-top: 1em; width: 750px; height: auto; max-width: 100%;" />
+		<?php /* <img src="<?php echo get_stylesheet_directory_uri(); ?>/assets/images/interface/supporters-2026.png" alt="Our partners" style="padding-top: 1em; width: 750px; height: auto; max-width: 100%;" /> */ ?>
 
 	</div><!-- sponsor_logos -->
 
